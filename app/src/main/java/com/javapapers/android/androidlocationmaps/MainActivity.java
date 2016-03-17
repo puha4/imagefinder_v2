913311841892fa96429662926f6e38ddbdffd110
Vitaliy Pugach <vitalick.pugach@gmail.com>
2016-03-17 10:59:04 +0200

    map in fragment

---
+++ app/src/main/java/com/javapapers/android/androidlocationmaps/MainActivity.java
@@ -0,0 +1,84 @@
+package com.javapapers.android.androidlocationmaps;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import com.javapapers.android.androidlocationmaps.api.FlickrPhotos;
+import com.javapapers.android.androidlocationmaps.api.Photo;
+import com.javapapers.android.androidlocationmaps.api.SearchApi;
+import com.javapapers.android.androidlocationmaps.fragment.GoogleMapFragment;
+import retrofit.*;
+
+public class MainActivity extends FragmentActivity implements
+        Callback<FlickrPhotos> {
+
+    private static final String TAG = "MainActivity";
+
+    private double latitude;
+    private double longitude;
+
+    private FragmentManager manager;
+    private GoogleMapFragment googleMapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate ...............................");
+
+        setContentView(R.layout.activity_location_google_map);
+
+        manager = getSupportFragmentManager();
+        googleMapFragment = new GoogleMapFragment();
+
+        if (savedInstanceState == null) {
+            manager.beginTransaction().add(R.id.mapContainer, googleMapFragment).commit();
+        }
+
+    }
+
+    // retrofit
+    public void onSearchClick(View view) {
+
+//        Gson gson = new GsonBuilder().create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://api.flickr.com")
+                .build();
+
+
+        SearchApi searchApi = retrofit.create(SearchApi.class);
+
+
+        this.latitude = googleMapFragment.getLatitude();
+        this.longitude = googleMapFragment.getLongitude();
+        Log.i(TAG, latitude+" "+ longitude);
+
+        Call<FlickrPhotos> call = searchApi.getPhotos(latitude, longitude);
+
+//        try {
+//            Response<FlickrPhotos> response = call.execute();
+//            Log.i(TAG, latitude+" "+ longitude);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        call.enqueue(this);
+    }
+
+    @Override
+    public void onResponse(Response<FlickrPhotos> response, Retrofit retrofit) {
+        Log.i(TAG, response.toString()+ " ");
+        for (Photo photo : response.body().photos.getPhoto()) {
+            Log.i(TAG, photo.toString());
+        }
+    }
+
+    @Override
+    public void onFailure(Throwable t) {
+        Toast.makeText(this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/javapapers/android/androidlocationmaps/fragment/GoogleMapFragment.java
@@ -0,0 +1,177 @@
+package com.javapapers.android.androidlocationmaps.fragment;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.ui.IconGenerator;
+import com.javapapers.android.androidlocationmaps.R;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class GoogleMapFragment extends Fragment implements
+        LocationListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String TAG = "GoogleMapFragment";
+
+    private View view;
+
+    private GoogleApiClient mGoogleApiClient;
+    private GoogleMap googleMap;
+    private LocationRequest mLocationRequest;
+    private Location mCurrentLocation;
+    private String mLastUpdateTime;
+    private double latitude;
+
+    private double longitude;
+    private static final long INTERVAL = 1000 * 60 * 10; //1 minute
+
+    private static final long FASTEST_INTERVAL = 1000 * 60 * 1; // 1 minute
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.google_map_fragment, container, false);
+
+        createLocationRequest();
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        SupportMapFragment fm = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+        googleMap = fm.getMap();
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+
+        return view;
+    }
+
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        addMarker();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected()) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "Connection failed: " + connectionResult.toString());
+    }
+
+    protected void startLocationUpdates() {
+        PendingResult<Status> pendingResult =
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    private void addMarker() {
+        googleMap.clear();
+
+        MarkerOptions options = new MarkerOptions();
+
+        IconGenerator iconFactory = new IconGenerator(getActivity());
+        iconFactory.setStyle(IconGenerator.STYLE_PURPLE);
+        options.icon(BitmapDescriptorFactory.fromBitmap(iconFactory.makeIcon(mLastUpdateTime)));
+        options.anchor(iconFactory.getAnchorU(), iconFactory.getAnchorV());
+
+
+        this.latitude = mCurrentLocation.getLatitude();
+        this.longitude = mCurrentLocation.getLongitude();
+
+        LatLng currentLatLng = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+        options.position(currentLatLng);
+//        Marker mapMarker = googleMap.addMarker(options);
+
+
+        long atTime = mCurrentLocation.getTime();
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date(atTime));
+
+        Marker mapMarker = googleMap.addMarker(new MarkerOptions()
+                .position(currentLatLng)
+                .title(mLastUpdateTime)
+                .snippet("You are here")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+//        mapMarker.setTitle(mLastUpdateTime);
+        Log.d(TAG, "Marker added.............................");
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 13));
+        Log.d(TAG, "Zoom done.............................");
+    }
+
+    protected void stopLocationUpdates() {
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+}
