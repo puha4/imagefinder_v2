bf5b30592f9529a39d552393880da5e0ab83f0aa
Yana <Yana>
2016-03-20 19:41:49 +0200

    flickr auth

--- app/src/main/java/com/javapapers/android/androidlocationmaps/MainActivity.java
+++ app/src/main/java/com/javapapers/android/androidlocationmaps/MainActivity.java
@@ -1,5 +1,7 @@
 package com.javapapers.android.androidlocationmaps;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -15,14 +17,19 @@ import com.javapapers.android.androidlocationmaps.api.Photo;
 import com.javapapers.android.androidlocationmaps.api.SearchApi;
 import com.javapapers.android.androidlocationmaps.fragment.GoogleMapFragment;
 import com.squareup.picasso.Picasso;
+
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
+
 import retrofit.*;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements
-        Callback<FlickrPhotos> {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
@@ -51,7 +58,14 @@ public class MainActivity extends AppCompatActivity implements
         }
 
         quiltView = (QuiltView) findViewById(R.id.quilt);
-        quiltView.setChildPadding(0);
+        quiltView.setChildPadding(1);
+
+        Uri uri = getIntent().getData();
+
+        if (uri != null) {
+            String frob = uri.getQueryParameter("frob");
+            Log.i(TAG+"frob", frob);
+        }
     }
 
     private void initToolbar() {
@@ -87,35 +101,57 @@ public class MainActivity extends AppCompatActivity implements
 
         Call<FlickrPhotos> call = searchApi.getPhotos(latitude, longitude);
 
-        call.enqueue(this);
+        call.enqueue(new Callback<FlickrPhotos>() {
+            @Override
+            public void onResponse(Response<FlickrPhotos> response, Retrofit retrofit) {
+                List<Photo> photos = response.body().getPhotos().getPhoto();
+                Collections.shuffle(photos);
+                List<Photo> subPhotos = photos.subList(0, 15);
+
+                ArrayList<ImageView> images = new ArrayList<ImageView>();
+
+                for (Photo photo : subPhotos) {
+                    ImageView image = new ImageView(MainActivity.this);
+                    image.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+                    Picasso.with(getApplicationContext())
+                            .load("https://farm" + photo.getFarm() + ".staticflickr.com/" + photo.getServer() + "/" + photo.getId() + "_" + photo.getSecret() + ".jpg")
+                            .fit()
+                            .centerInside()
+                            .into(image);
+
+                    images.add(image);
+                }
+
+                quiltView.addPatchImages(images);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        String apiSig2 = new String(Hex.encodeHex(DigestUtils.md5("6e732ab8487b05e4api_key5f45c46eaf6e87b55c9f36fec03e3466permswrite")));
+
+        Log.i(TAG + "apiSig2", apiSig2);
+
+//        Call<String> callAuth = searchApi.doAuth(apiSig2);
+//        callAuth.enqueue(new Callback<String>() {
+//            @Override
+//            public void onResponse(Response<String> response, Retrofit retrofit) {
+//                Log.i(TAG+"auth", response.toString());
+//                Log.i(TAG+"auth", response.body().toString());
+//            }
+//
+//            @Override
+//            public void onFailure(Throwable t) {
+//                Toast.makeText(MainActivity.this, "auth "+t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+//            }
+//        });
     }
 
-    @Override
-    public void onResponse(Response<FlickrPhotos> response, Retrofit retrofit) {
-        List<Photo> photos = response.body().getPhotos().getPhoto();
-        Collections.shuffle(photos);
-        List<Photo> subPhotos = photos.subList(0, 15);
-
-        ArrayList<ImageView> images = new ArrayList<ImageView>();
-
-        for (Photo photo : subPhotos) {
-            ImageView image = new ImageView(this.getApplicationContext());
-            image.setScaleType(ImageView.ScaleType.CENTER_CROP);
-
-            Picasso.with(getApplicationContext())
-                    .load("https://farm"+photo.getFarm()+".staticflickr.com/"+photo.getServer()+"/"+photo.getId()+"_"+photo.getSecret()+".jpg")
-                    .fit()
-                    .centerInside()
-                    .into(image);
-
-            images.add(image);
-        }
-
-        quiltView.addPatchImages(images);
-    }
-
-    @Override
-    public void onFailure(Throwable t) {
-        Toast.makeText(this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+    public void onAuth(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.flickr.com/services/auth/?api_key=5f45c46eaf6e87b55c9f36fec03e3466&perms=write&api_sig=9e61f86f3d3f8caf024a8bd1729d4ff3")));
     }
 }
--- app/src/main/java/com/javapapers/android/androidlocationmaps/api/SearchApi.java
+++ app/src/main/java/com/javapapers/android/androidlocationmaps/api/SearchApi.java
@@ -6,6 +6,9 @@ import retrofit.http.Query;
 
 public interface SearchApi {
 
+    @GET("/services/auth/?api_key=5f45c46eaf6e87b55c9f36fec03e3466&format=json&nojsoncallback=1&perms=write")
+    Call<String> doAuth(@Query("api_sig") String apiSig);
+
     @GET("/services/rest/?method=flickr.photos.search&api_key=5f45c46eaf6e87b55c9f36fec03e3466&format=json&nojsoncallback=1")
     Call<FlickrPhotos> getPhotos(
             @Query("lat") double latitude,
